61a23b643a0da41b041cd045000f39a20a4084f8
Prajwal-Pardhi <prajwalpardhi19@gmail.com>
2024-03-09 00:09:47 +0530

    sidebar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { IoMenu } from "react-icons/io5";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
 // import '../Navbar.css'; // Import custom CSS for navbar styling
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-      };
-    const headings = ["THE REALM OF SUSTAINABILITY", "WHATS FAST IN FASHION", "WHAT'S HOT IN THE SNEAKER WORLD", "ALL IN LUXURY"];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const headings = [
+    "THE REALM OF SUSTAINABILITY",
+    "WHATS FAST IN FASHION",
+    "WHAT'S HOT IN THE SNEAKER WORLD",
+    "ALL IN LUXURY",
+  ];
   return (
     <nav className="navbar">
-        <div className="navbar-container">
-        <div className="left-part">
-            {/* Logo */}
-            {/* <img className='nav-logo-img' src="logo.jpeg" alt="logo-img" /> */}
-            
-            <div className="menu-container">
-                {/* Menu button */}
-                <IoMenu className="menu-icon" onClick={toggleMenu} size={30}/>
-                {/* Side window */}
-                {isMenuOpen && (
-                    <div className="side-window">
-                    <ul>
-                        <li><Link to="/about_us" style={{textDecoration:"none", color:"white"}}>About Us</Link></li>
-                        <li><Link to="/contact_us" style={{textDecoration:"none", color:"white"}}>Contact</Link></li>
-                        <li><Link to="/" style={{textDecoration:"none", color:"white"}}>Newsletter</Link></li>
-                    </ul>
-                    </div>
-                )}
-            </div>
-            <div className="nav-title-name">LIT</div>
+        <Sidebar showMenu={isMenuOpen} setShowMenu={setIsMenuOpen} />
+      <div className="navbar-container">
+        <div className="left-part" onClick={toggleMenu}>
+
+          <div className="menu-container">
+
+            <IoMenu className="menu-icon" size={30} />
+          </div>
+          <div className="nav-title-name">LIT</div>
         </div>
         <div className="mid-part">
-            <ul className="nav-links">
+          <ul className="nav-links">
             {/* Options on the right */}
-            <li><Link to={`/blog/${headings[0] }`}>Sustainability</Link></li>
-            <li><Link to={`/blog/${headings[1] }`}>Fast Fashion</Link></li>
-            <li><Link to={`/blog/${headings[2] }`}>Sneaker</Link></li>
-            <li><Link to={`/blog/${headings[3] }`}>Luxury</Link></li>
-            </ul>
+            <li>
+              <Link to={`/blog/${headings[0]}`}>Sustainability</Link>
+            </li>
+            <li>
+              <Link to={`/blog/${headings[1]}`}>Fast Fashion</Link>
+            </li>
+            <li>
+              <Link to={`/blog/${headings[2]}`}>Sneaker</Link>
+            </li>
+            <li>
+              <Link to={`/blog/${headings[3]}`}>Luxury</Link>
+            </li>
+          </ul>
         </div>
         <div className="right-part">
-            <button className='nav-subscribe-btn'>Subscribe</button>
+          <button className="nav-subscribe-btn">Subscribe</button>
         </div>
       </div>
-      
     </nav>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { RxCross2 } from "react-icons/rx";
+import '../Sidebar.css';
+import {
+    FaYoutube,
+    FaInstagram,
+    FaFacebook,
+    FaLinkedin,
+    FaTwitter,
+  } from "react-icons/fa";
+
+const Sidebar = ({ showMenu, setShowMenu }) => {
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className={`side-window ${showMenu ? 'show-sidebar' : ''}`}>
+      
+      <div className="close-button" onClick={handleClick}>
+        <RxCross2 className="text-white" />
+        <p className="text-lit">LIT</p>
+      </div>
+      <ul>
+        <li>
+          <Link
+            to="/about_us"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact_us"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Newsletter
+          </Link>
+        </li>
+      </ul>
+
+      <div className="title-and-icons" style={{transform: "scale(0.5)",}}>
+          <div className="blog-social-title">Luxury In Taste - LIT</div>
+          <div className="social-icons">
+            <div className="icons-in-sidebar">
+            <div className="single-icon">
+              <Link to="https://www.youtube.com" target="_blank">
+                <FaYoutube style={{ fontSize: "30px", color: "red" }} />
+              </Link>
+            </div>
+            <div className="single-icon">
+              <Link to="#" target="_blank">
+                <FaFacebook style={{ fontSize: "30px", color: "#3b5998" }} />
+              </Link>
+            </div>
+            <div className="single-icon">
+              <Link
+                to="https://www.instagram.com/luxuryintaste?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                target="_blank"
+              >
+                <FaInstagram style={{ fontSize: "30px", color: "#C13584" }} />
+              </Link>
+            </div>
+            <div className="single-icon">
+              <Link to="#" target="_blank">
+                <FaTwitter style={{ fontSize: "30px", color: "#1DA1F2" }} />
+              </Link>
+            </div>
+            <div className="single-icon">
+              <Link
+                to="https://www.linkedin.com/company/luxury-in-taste-lit/"
+                target="_blank"
+              >
+                <FaLinkedin style={{ fontSize: "30px", color: "#0077B5" }} />
+              </Link>
+            </div>
+          </div>
+          </div>
+        </div>
+
+    </div>
+  );
+};
+
+export default Sidebar;
